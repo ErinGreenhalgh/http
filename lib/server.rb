23fe657898521a004fb8360d4bef7286bd738568
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-04-12 17:06:52 -0600

    working on formating request lines into a hash

--- lib/server.rb
+++ lib/server.rb
@@ -10,7 +10,7 @@ class Server
     @port = port
     @counter = 0
   end
-  # 
+  #
   # def create_client
   #   @tcp_server.accept
   # end
@@ -39,7 +39,7 @@ class Server
   def format_response
     response = "<pre>" + "Hello World  (#{@counter/2})\n" + "</pre>"
     # response = "<pre>" + request_lines.join("\n") + "</pre>"
-    output = "<html><head><title></title></head><body>#{response}</body></html>"
+    output = "<html><head></head><body>#{response}</body></html>"
   end
 
   def headers
@@ -53,5 +53,16 @@ class Server
   def respond
     @client.puts headers
     @client.puts format_response
+  end
+
+  def format_first_request_line
+    #don't have a way to access request_lines 
+    hash = {}
+    first_line = request_lines[0].split(" ")
+    #find first line the the array, split it
+    hash["Verb"] = first_line[0] #GET
+    hash["Path"] = first_line[1] # /
+    hash["Protocol"] = first_line[2] #HTTP/1.1
+    hash
   end
 end
--- test/server_test.rb
+++ test/server_test.rb
@@ -5,17 +5,21 @@ require 'pry'
 
 class ServerTest < Minitest::Test
 
+  def setup
+    server = Server.new
+  end
+
   def test_it_can_listen_to_a_specific_port
     server = Server.new
     assert_equal 9292, server.port
   end
 
-  def test_it_can_receive_a_request
-    server = Server.new
-    assert nil, server.receive_request #it received it
-    #if the request_lines array has something in it,
-    #it has received a request
-  end
+  # def test_it_can_receive_a_request
+  #   server = Server.new
+  #   assert nil, server.receive_request #it received it
+  #   #if the request_lines array has something in it,
+  #   #it has received a request
+  # end
 
   def test_it_can_send_response
     server = Server.new
@@ -26,6 +30,11 @@ class ServerTest < Minitest::Test
     content-length: 123 \n
 
     Hello World (0)", server.respond
+  end
+
+  def test_it_can_format_first_request_line
+    assert_equal { "Verb" => "GET", "Path" => "/", "Protocol" => "HTTP/1.1"},
+    server.format_first_request_line
   end
 
 end
